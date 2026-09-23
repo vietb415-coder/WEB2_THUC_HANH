67b061eb4b21385bae1892746dfad245a0134078
dong lee <vietb415@gmail.com>
2026-09-23 15:20:48 +0700

    Add project files.

---
+++ WEB2_THUC_HANH/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB2_THUC_HANH.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
